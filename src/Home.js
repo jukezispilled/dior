54e312625f38ff86659e3eda5eb009d266c1e7bc
Jack Anderson <whotfisjax@gmail.com>
2024-06-16 19:39:54 -0400

    v0

---
+++ src/Home.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import './App.css';
+
+function Home() {
+  return (
+    <div
+      className="h-screen w-screen flex justify-center items-center font-custom text-5xl md:text-8xl bg-cover bg-center"
+      style={{ backgroundImage: "url('/Dior2.png')" }}
+    >
+      <div className='absolute top-[10%]'>
+        DIOR DOGE
+      </div>
+      <a href="/products" className='absolute bottom-[15%] font-custom1 text-2xl md:text-3xl bg-white w-min px-3 py-2 border whitespace-nowrap'>
+        SHOP NOW
+      </a>
+    </div>
+  );
+}
+
+export default Home;
---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}",
+  ],
+  theme: {
+    fontFamily: {
+      custom: ['CustomFont', 'sans-serif'],
+      custom1: ['CustomFont1', 'sans-serif'],
+    },
+    extend: {},
+  },
+  plugins: [],
+}
+
